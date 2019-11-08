bb64783e76f8f649bc5a9b01ee0827b60b47f9a4
Joshlan Raposo <1801686@brunel.ac.uk>
2019-11-08 19:01:38 +0000

    Part 2A Level 4 Completed With some changes to overal code

--- app/src/main/java/com/example/biggernumber/MainActivity.java
+++ app/src/main/java/com/example/biggernumber/MainActivity.java
@@ -6,13 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity { //implements View.OnClickListener {
 
     TextView tv, tv2;
-    int number, number1, points = 1;
+    int number, number1, points = 0, lives = 3;
 
     public static int randomNumber() { // Generating random number for 2 different buttons
         Random rand = new Random();
@@ -27,13 +28,19 @@ public class MainActivity extends AppCompatActivity { //implements View.OnClickL
     }
 
     public void setTheButtons() {
-        number = randomNumber();
-        Button left = this.findViewById(R.id.LEFT1);
-        left.setText(Integer.toString(number));
 
-        number1 = randomNumber();
-        Button right = this.findViewById(R.id.RIGHT1);
-        right.setText(Integer.toString(number1));
+        if (lives == 0){
+            lives = 3;
+            Toast.makeText(this, "Game Over", Toast.LENGTH_LONG).show();
+        } else {
+            number = randomNumber();
+            Button left = this.findViewById(R.id.LEFT1);
+            left.setText(Integer.toString(number));
+
+            number1 = randomNumber();
+            Button right = this.findViewById(R.id.RIGHT1);
+            right.setText(Integer.toString(number1));
+        }
 
     }
 
@@ -41,12 +48,18 @@ public class MainActivity extends AppCompatActivity { //implements View.OnClickL
     public void guessTheBiggerNumberLeft(View view) {
         tv = this.findViewById(R.id.displayText);
         if (number > number1) {
-            tv.setText("Congratulations " + " Points : " + points);
             points++;
             setTheButtons();
+            tv.setText("Congratulations" + "/Points: " + points + "/Lives: " + lives );
+
+
+        } else if (number < number1){
+            lives--;
+            setTheButtons();
+            tv.setText("Wrong" + "/Lives: " + lives );
+
 
         } else {
-            tv.setText("Wrong");
             setTheButtons();
         }
     }
@@ -55,12 +68,18 @@ public class MainActivity extends AppCompatActivity { //implements View.OnClickL
     public void guessTheBiggerNumberRight(View view) {
         tv = this.findViewById(R.id.displayText);
         if (number1 > number) {
-            tv.setText("Congratulations" + "/Points : " + points);
             points++;
             setTheButtons();
+            tv.setText("Congratulations" + "/Points: " + points + "/Lives: " + lives );
+
+
+        } else if (number1 < number){
+            lives--;
+            setTheButtons();
+            tv.setText("Wrong" + "/Lives: " + lives );
+
 
         } else {
-            tv.setText("Wrong");
             setTheButtons();
         }
     }
